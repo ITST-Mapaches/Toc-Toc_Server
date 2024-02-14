cef688f2c8286bd5a7fadabae9e55b8a0bf92acc
Humberto Domínguez <humbertodelac13@gmail.com>
2024-02-13 21:34:50 -0600

    comentarios agregados en el código

--- src/KnockKnockProtocol.java
+++ src/KnockKnockProtocol.java
@@ -4,26 +4,29 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class KnockKnockProtocol {
-    //? constantes que permiten saber el punto en que se encuentra la broma
+    //? constantes que permiten saber el punto en que se encuentra la(s) broma(s)
+    //? constante: representa la espera del programa
     private static final int WAITING = 0;
-    //? solo se va a enviar un knock knock
+    //? constante: representa el envío de toc toc
     private static final int SENTKNOCKKNOCK = 1;
-    //? enviar
+    //? constante: representa el envío de una pista
     private static final int SENTCLUE = 2;
+    //? constante: representa el punto en que le preguntamos al cliente si quiere otra broma
     private static final int ANOTHER = 3;
-
+    //? constante: representa el numero de bromas que hay en el programa
     private static final int NUMJOKES = 5;
-
+    //? variable / atributo: representa el estado actual de la broma, en un inicio está esperando
     private int state = WAITING;
+    //? variable / atributo: representa el número de la broma actual
     private int currentJoke = 0;
 
-    //! Lista con pistas
+    //! atributo / variable: Lista que contendrá las pistas de las bromas
     List<String> pistas;
-    //! Lista con respuestas
+    //! atributo / variable: Lista que contendrá las respuestas de las pistas, la broma en sí
     List<String> respuestas;
 
 
-    //!Constructor vacío
+    //!Constructor vacío, en el se inicializa el arraylist y luego se agrega elementos según corresponda
     public KnockKnockProtocol() {
         //!inicialización de arraylists pistas
         this.pistas = new ArrayList<>();
@@ -45,7 +48,7 @@ public class KnockKnockProtocol {
         this.pistas.add("Llito");
     }
 
-    //!Método que agrega respuestas a la lista de respuestas
+    //!Método que agrega respuestas a la lista de respuestas a las pistas
     public void agregaRespuestas() {
         this.respuestas.add("El que te vio hace ratito");
         this.respuestas.add("Agua, por favor");
@@ -54,72 +57,75 @@ public class KnockKnockProtocol {
         this.respuestas.add("yo, pollito!");
     }
 
-
     /**
-     * ? Método que
+     * ? Método que procesa la entrada / respuesta del cliente
      *
-     * @param theInput
-     * @return
+     * @param theInput respuesta del cliente
+     * @return respuesta del propio servidor
      */
     public String processInput(String theInput) {
+        //? cadena que representa la respuesta del servidor, que es el retorno del método
         String theOutput = null;
 
-        //? si el estado esta en esperando, envia la frase y el estado cambia
+        //? si el estado esta en 'esperando', entonces envia la frase 'toc toc' y el estado cambia a "toc toc enviado"
         if (state == WAITING) {
             theOutput = "toc! toc!";
             state = SENTKNOCKKNOCK;
 
-            //? si ya se envío la frase, es decir si ya dijmos knock knock
+            //? si el estado es 'toc toc enviado' (ya se envío un toc toc), entonces
         } else if (state == SENTKNOCKKNOCK) {
-            //? si la entrada (lo que escribió el cliente) es who's there?
+            //? si la entrada (lo que escribió el cliente) es quien es?, entonces
             if (theInput.equalsIgnoreCase("Quien es?")) {
-                //? entonces se envía el primer elemento de clues 'Turnip'
-//                theOutput = clues[currentJoke];
+                //? se envía la pista de la broma actual
                 theOutput = pistas.get(currentJoke);
-                //?el estado cambia a pista enviada
+                //? el estado cambia a 'pista enviada'
                 state = SENTCLUE;
 
-                //? si no es asi (si el cliente no escribe 'Who's there?') la respuesta es "se supone que debiste haber dicho who's there"
+                //? si no es asi (si el cliente no escribe 'quien es?'), la respuesta del servidor es:
             } else {
-                theOutput = "debiste decir \"Quien es?\" " +
-                        "Prueba de nuevo. toc! toc!";
+                theOutput = "debiste decir \"Quien es?\", Prueba de nuevo." +  "\nToc! Toc!";
             }
 
-            //? si es estado es igual a : (si ya se envió una pista)
+            //? si es estado es igual a 'pista enviada' (ya se envío una pista previamente), entonces
         } else if (state == SENTCLUE) {
             // ? si la respuesta del cliente es igual a la pista que le envío + ?
             if (theInput.equalsIgnoreCase(pistas.get(currentJoke) + "?")) {
-                //? la respuesta de este es el primer elemento del array answers y le pregunta si quiere otra broma
-                theOutput = respuestas.get(currentJoke) + " Quieres otra super broma? (y/n)";
-                //? el estado es 'otra', es decir que el cliente quiere otra broma
+                //? la respuesta del servidor es la respuesta de la pista y le pregunta si quiere otra broma
+                theOutput = respuestas.get(currentJoke) + ", Quieres otra super broma? (y/n)";
+                //? el estado cambia a 'otra'
                 state = ANOTHER;
 
-                //? si no responde lo esperado entonces le dice que se supone que debió haber constestado x
+                //? si no responde lo esperado entonces le dice que se supone que debió haber constestado:
             } else {
-                theOutput = "Debiste decir \"" +
-                        pistas.get(currentJoke) +
-                        " ?\"" +
-                        "Prueba de nuevo. Toc! Toc!";
-                //? el estado de nuevo es enviar knock kcnok
+                theOutput = "Debiste decir \"" + pistas.get(currentJoke) + "?\", " + "Prueba de nuevo. \nToc! Toc!";
+                //? el estado de nuevo 'toc toc enviado'
                 state = SENTKNOCKKNOCK;
             }
 
-            //? si el estado es 'otra' es decir si el usuario quiere otra broma
+            //? si el estado es 'otra', entonces
         } else if (state == ANOTHER) {
-            //? si la respuesta del cliente es y, es decir que sí quiere otra broma
+            //? si la respuesta del cliente es 'y' es decir, que sí quiere otra broma, entonces
             if (theInput.equalsIgnoreCase("y")) {
+                //? enviamos toc toc
                 theOutput = "Toc! Toc!";
+                //? si la broma actual es igual a 4, que es el índice final de las bromas (de pruebas / respuestas), entonces iniciamos de nuevo
                 if (currentJoke == (NUMJOKES - 1))
                     currentJoke = 0;
+                // ? si no es la última broma, avanzamos de broma
                 else
                     currentJoke++;
+                //? el estado de nuevo 'toc toc enviado'
                 state = SENTKNOCKKNOCK;
-                //? si el usuario no quiere otra broma la respuesta de este es adios
+
+                //? si el cliente no quiere otra broma la respuesta del servidor es: 'adios'
             } else {
                 theOutput = "Adios!.";
+                //? el estado cambia a esperando
                 state = WAITING;
             }
         }
+
+        //? retornamos la salida
         return theOutput;
     }
 }
--- src/KnockKnockServer.java
+++ src/KnockKnockServer.java
@@ -4,56 +4,55 @@ import java.io.*;
 public class KnockKnockServer {
     public static void main(String[] args) throws IOException {
 
-        //valida si se recibe un argumento en tiempo de ejecución
+        //? valida si se recibe un argumento en tiempo de ejecución
         if (args.length != 1) {
             System.err.println("Usage: java KnockKnockServer <port number>");
             System.exit(1);
         }
 
-        //casteamos el argumentos a entero, este será el puerto por el que escuchará el socket servidor
+        //? casteamos el argumentos a entero, este será el puerto por el que escuchará el socket servidor
         int portNumber = Integer.parseInt(args[0]);
 
         try (
-                //crea un socket de servidor que va a esuchar en el puerto indicado
+                //? crea un socket de servidor que va a esuchar en el puerto indicado
                 ServerSocket serverSocket = new ServerSocket(portNumber);
 
-                //crea un socket cliente,
+                //? crea un socket cliente,
                 Socket clientSocket = serverSocket.accept();
 
-                //objeto de flujo de salida, por medio de este vamos a enviar información al socket cliente
+                //? objeto de flujo de salida, por medio de este vamos a enviar información al socket cliente
                 PrintWriter out =
                         new PrintWriter(clientSocket.getOutputStream(), true);
 
-                //objeto de flujo de entrada, por medio de este vamos a leer lo que el socket cliente envíe
+                //? objeto de flujo de entrada, por medio de este vamos a leer lo que el socket cliente envíe
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(clientSocket.getInputStream()));
         ) {
 
-            //cadenas
+            //? cadenas una representa la entrada por consola y otra la salida (envio de mensajes al cliente)
             String inputLine, outputLine;
 
-            // Initiate conversation with client
+            //? intancia que permite determinar el mensaje a enviar al cliente, dependiendo de la respuesta del mismo
             KnockKnockProtocol kkp = new KnockKnockProtocol();
 
-            //se guarda el knock knock
+            //? determinamos la respuesta del servidor, por medio del método (el primer mensaje es 'Toc Toc')
             outputLine = kkp.processInput(null);
 
-            //envía el knock knock a través del flujo de salida
+            //? envía la respuesta del servidor, través del flujo de salida
             out.println(outputLine);
 
-            //!mientras se lea en consola algo que no sea nulo
+            //!mientras se lea en el flujo de entrada algo que no sea nulo
             while ((inputLine = in.readLine()) != null) {
-                //recibimos la resuespta del método processInput
+                //? recibimos la resuespta del servidor por medio del metodo y le envíamos la respuesta del cliente
                 outputLine = kkp.processInput(inputLine);
 
-                //enviamos la respuesta al cliente por el flujo de salida
+                //? enviamos la respuesta al cliente por el flujo de salida
                 out.println(outputLine);
 
 
-                //si el método processInput retorna un 'Bye' salimos el ciclo
+                //? si el método processInput retorna un 'Bye' salimos el ciclo
                 if (outputLine.equals("Bye."))
                     break;
-
             }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
